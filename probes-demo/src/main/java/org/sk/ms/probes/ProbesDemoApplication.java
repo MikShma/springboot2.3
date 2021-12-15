bf93a9615ad8b3a98ca1ec76e66f5cff2155f60e
MikShma <67111851+MikShma@users.noreply.github.com>
2021-12-15 11:56:38 +0300

    Update ProbesDemoApplication.java

--- probes-demo/src/main/java/org/sk/ms/probes/ProbesDemoApplication.java
+++ probes-demo/src/main/java/org/sk/ms/probes/ProbesDemoApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.availability.ApplicationAvailabilityBean;
 import org.springframework.boot.availability.LivenessState;
 import org.springframework.boot.availability.ReadinessState;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProbesDemoApplication {
+public class ProbesDemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProbesDemoApplication.class, args);
